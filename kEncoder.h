140d927ea2d5d53961f30bc24ea69f6a1b40b07e
Keir Rice <keirrice@gmail.com>
2018-12-12 22:24:36 +1300

    Fixing up git issues

--- kEncoder.h
+++ kEncoder.h
@@ -6,149 +6,188 @@
 
 #include <Arduino.h>
 #include <assert.h>
-#include <kPin.h>
-
-using namespace kPin;
-
-namespace kEncoder {
-
-typedef volatile uint8_t RoReg; /**< Read only 8-bit register (volatile const unsigned int) */
-typedef volatile uint8_t RwReg; /**< Read-Write 8-bit register (volatile unsigned int) */
-typedef volatile RwReg * port_ptr_t;
-typedef RwReg port_t;
-
-uint8_t maskToShifter(uint8_t mask);
 
 
+namespace kEncoder{
 
-/*Interface for the pin collections*/
-class PinCollectionInterface {
-  public:
-  	uint8_t mPinCount = 0;
-  	PinID pins[] = {};
+	typedef volatile uint8_t RoReg; /**< Read only 8-bit register (volatile const unsigned int) */
+	typedef volatile uint8_t RwReg; /**< Read-Write 8-bit register (volatile unsigned int) */
+	typedef volatile RwReg * port_ptr_t;
+	typedef RwReg port_t;
 
-    virtual byte read();
-    virtual void pinMode(uint8_t);
+	uint8_t maskToShifter(uint8_t mask);
 
-};
+	
 
+	/*Interface for the pin collections*/
+	class PinCollectionInterface {
+		public:
+			uint8_t mPinCount;
+			uint8_t pins[];
+			virtual byte read();
+			virtual void pinMode(uint8_t);
 
-template<uint8_t... Pins> class PinGroup : public PinCollectionInterface {
-  public:
-    uint8_t mPinCount = sizeof...(Pins);
-    PinID pins[sizeof...(Pins)] = {PinID(Pins)...};
+	};
 
-    virtual byte read() override {
-      byte value = 0;
-      for (uint8_t i = 0; i < this->mPinCount; ++i) {
-        value |= this->pins[i].digitalRead() << i;
-      }
-      return value;
-    };
+	template<uint8_t... Pins> class _PinCollection : public PinCollectionInterface {
+		public:
+		  uint8_t mPinCount = sizeof...(Pins);
+		  uint8_t pins[sizeof...(Pins)] = {uint8_t(Pins)...};
+	};
 
-    virtual void pinMode(uint8_t mode) override {
-      for (uint8_t i = 0; i < this->mPinCount; ++i) {
-        this->pins[i].pinMode(mode);
-      }
-    };
-};
+	template<uint8_t... Pins> class PinBank : public _PinCollection<Pins...>{
+		public:
+			uint8_t mPinCount;
+			uint8_t pins[];
+			
+		  RwReg sPinMask;
+		  volatile RoReg *sInPort;
+		  uint8_t sPortShiftOnRead;
 
-class PinPort : public PinCollectionInterface {
-  public:
+		  /* Compile time assert that pins are contiguous. */
+		  template<class T, class... Args>
+		  constexpr static bool check(T arg1, T arg2){
+		    return (bool)(arg1 + 1 == arg2);
+		  };
+		  template<class T, class... Args>
+		  constexpr static bool check(T arg1, T arg2, Args... args){
+		    return (bool) ((arg1 + 1 == arg2) & check(arg2, args...));
+		  };
+		  static_assert(check(Pins...), "Invalid pins specified. We need pins in a contigous range on the same port.");
 
-    const PortID &mPort;
-    uint8_t mMask;
-    uint8_t mShift;
+		  PinBank() {
+		    _init(Pins...);
+		    sPortShiftOnRead = maskToShifter(sPinMask);
+		  }
 
-    PinPort(const PortID &port, uint8_t mask) : mPort(port), mMask(mask) {
-      mShift = maskToShifter(mask);
-    }
+		  /* Build up port data. */
+		  void _init(int counter=0){};
+		  template<class T, class... Args>
+		  void _init(T arg, Args... args){
+		  	int counter=0;
+		    if(counter == 0){
+		      // Set these values once, then we can just check the reset of the pins match.
+		      sInPort = portInputRegister(digitalPinToPort(arg));
+		    }
+		    else {
+		      // Fail if any of the pins are on a diffrent port.
+		      assert(sInPort == portInputRegister(digitalPinToPort(arg)));
+		    }
+		    // Accumulate the bit mask
+		    sPinMask |= digitalPinToBitMask(arg);
+		    _init(args..., ++counter);
+		  };
 
-    inline virtual uint8_t read() override {
-      return mPort.digitalRead(mMask, mShift);
-    };
-    virtual void pinMode(uint8_t mode) override {
-    	mPort.pinMode(mode, mMask);
-    };
-};
+		  virtual byte read() override {
+		  	return (*sInPort & sPinMask) << sPortShiftOnRead;
+		  };
+		  virtual void pinMode(uint8_t mode) override {
+		  	if(mode == INPUT_PULLUP){
+		  		volatile RwReg *sDDRPort = portModeRegister(digitalPinToPort(pins[0]));
+		  		volatile RwReg *sPort  = portOutputRegister(digitalPinToPort(pins[0]));
 
+			  	*sDDRPort |= ~sPinMask; // Set mask pins to input
+				*sPort |= sPinMask; // Set mask pins to pullup
+			}
+			else {
+				assert(false); // "todo"
+			}
+		  };
+	};
 
-class Encoder
-{
-  public:
+	template<uint8_t... Pins> class PinGroup : public _PinCollection<Pins...> {
+		public:
 
-    PinCollectionInterface *mPins;
+		  virtual byte read() override{
+		    byte value = 0;
+		    for(uint8_t i=0; i < this->mPinCount; ++i){
+		      value |= ::digitalRead(this->pins[i]) << i;
+		    }
+		    return value;
+		  };
 
-    Encoder() : mPins(nullptr) {};
-    Encoder(PinCollectionInterface &pins) : mPins(&pins) {};
-
-    void setPins(PinCollectionInterface &pins) {
-      mPins = &pins;
-    };  // We need all both pins next to each other for fast reads.
-    void setDebounceDelay(int delay);
-    void setInteruptHandler(void (*interuptHandler)(void));
-
-    void interputHandler();
-
-    bool error(bool reset_flag = true);
-    virtual void update(byte) {};
-
-  protected:
-    volatile bool error_flag = false;
-
-    unsigned int _debounce_delay = 0;
-
-    void (*_interupt_handler)(void);
-};
-
-class AbsoluteEncoder : public  Encoder
-{
-  public:
-
-    AbsoluteEncoder() : Encoder() {};
-    AbsoluteEncoder(PinCollectionInterface pins) : Encoder(pins) {};
-
-    void setup();
-    void setup(void (*interuptHandler)(void));
-
-    volatile char position = 0;
-    volatile char direction = 0;
-
-  private:
-    // Encoder state packed into a byte so we can use it as an index into the direction array.
-    // bits 7-4 == previous values
-    // bits 3-0 == current values
-    volatile byte _absolute_encoder_state;
-
-    void (*_interupt_handler)(void);
-
-    void update(byte new_reading);
-
-    typedef byte (AbsoluteEncoder::*fptr)(void);
-    fptr read_values;
-};
+		  virtual void pinMode(uint8_t mode) override{
+		    for(uint8_t i=0; i < this->mPinCount; ++i){
+		    	::pinMode(this->pins[i], mode);
+		    }
+		  };
+	};
 
 
-class RelativeEncoder : public Encoder {
-  public:
+	class Encoder
+	{
+	public:
 
-    RelativeEncoder() : Encoder() {};
-    RelativeEncoder(PinCollectionInterface pins) : Encoder(pins) {};
+		PinCollectionInterface *mPins;
 
-    void setup();
-    void setup(void (*interuptHandler)(void));
+		Encoder() : mPins(nullptr) {};
+		Encoder(PinCollectionInterface &pins) : mPins(&pins) {};
 
-    volatile char steps = 0;
-    volatile char direction = 0;
+		void setPins(PinCollectionInterface &pins){mPins = &pins;};  // We need all both pins next to each other for fast reads.
+		void setDebounceDelay(int delay);
+		void setInteruptHandler(void (*interuptHandler)(void));
 
-  private:
-    volatile byte _relative_encoder_state;
-    volatile char _relative_steps_buffer = 0;
+		void interputHandler();
 
-    void (*_interupt_handler)(void);
+		bool error(bool reset_flag = true);
+		virtual void update(byte){};
+		
+	protected:
+		volatile bool error_flag = false;
+		
+		unsigned int _debounce_delay = 0;
 
-    void update(byte new_reading);
-};
+		void (*_interupt_handler)(void);
+	};
+
+	class AbsoluteEncoder : public  Encoder
+	{
+	public:
+
+		AbsoluteEncoder() : Encoder() {};
+		AbsoluteEncoder(PinCollectionInterface pins) : Encoder(pins) {};
+
+		void setup();
+		void setup(void (*interuptHandler)(void));
+		
+		volatile char position = 0;
+		volatile char direction = 0;
+
+	private:
+		// Encoder state packed into a byte so we can use it as an index into the direction array.
+		// bits 7-4 == previous values
+		// bits 3-0 == current values
+		volatile byte _absolute_encoder_state;
+
+		void (*_interupt_handler)(void);
+
+		void update(byte new_reading);
+
+		typedef byte (AbsoluteEncoder::*fptr)(void);
+  		fptr read_values;
+	};
+
+
+	class RelativeEncoder : public Encoder {
+	public:
+
+		RelativeEncoder() : Encoder() {};
+		RelativeEncoder(PinCollectionInterface pins) : Encoder(pins) {};
+
+		void setup();
+		void setup(void (*interuptHandler)(void));
+
+		volatile char steps = 0;
+		volatile char direction = 0;
+
+	private:
+		volatile byte _relative_encoder_state;
+		volatile char _relative_steps_buffer = 0;
+		
+		void (*_interupt_handler)(void);
+
+		void update(byte new_reading);
+	};
 };
 
 
